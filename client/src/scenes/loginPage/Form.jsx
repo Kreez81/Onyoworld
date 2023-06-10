58a8eeca49018d6e22e5d7147bcee4fab28069ea
Karan Pawar <110679627+Kreez81@users.noreply.github.com>
2023-06-10 11:16:19 +0530

    testing

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -117,7 +117,7 @@ function Form () {
         handleBlur,
         handleChange,
         handleSubmit,
-        
+        setFieldValue,
         resetForm,
       }) => (
         <form onSubmit={handleSubmit}>
@@ -184,9 +184,9 @@ function Form () {
                   <Dropzone
                     acceptedFiles=".jpg,.jpeg,.png"
                     multiple={false}
-                    // onDrop={(acceptedFiles) =>
-                    //   setFieldValue("picture", acceptedFiles[0])
-                    // }
+                    onDrop={(acceptedFiles) =>
+                      setFieldValue("picture", acceptedFiles[0])
+                    }
                   >
                     {({ getRootProps, getInputProps }) => (
                       <Box
